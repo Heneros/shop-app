23516f088632d6cad7086db7dd297ce6290c82af
Heneros <punk60826@gmail.com>
2021-11-21 23:55:53 +0200

    added reducers and new components

--- shop-app/src/components/App.js
+++ shop-app/src/components/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {  Router} from 'react-router-dom';
+import {BrowserRouter, Route,  Router} from 'react-router-dom';
 import Header from './Header';
+import Product from './product/product';
+import AddNewProduct from './product/add-new-product';
+
+
 import Style from './Style.css';
 import history from './history';
+
 
 const App = () => {
     return(<div>
     <Router history={history}>
       <Header />
+      <Product/>
+      <AddNewProduct/>
     </Router>
 </div>
 );
---
+++ shop-app/src/components/product/add-new-product.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const AddNewProduct = () =>{
+    return <div className="ui container">Add New Product</div>
+}
+
+export default AddNewProduct;
