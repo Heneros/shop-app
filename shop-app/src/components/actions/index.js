d7d3b1889c6bbec844f45c76e85715ee60bc326a
Heneros <punk60826@gmail.com>
2021-12-04 23:03:46 +0200

    email from api output

--- shop-app/src/components/actions/index.js
+++ shop-app/src/components/actions/index.js
@@ -4,16 +4,15 @@ export const fetchProducts =  () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({ type: 'FETCH_PRODUCTS', payload: response.data});
- 
-   };
+};
+
 export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
 };
 
-
 export const fetchComment = id => async dispatch =>{
-  const response = await jsonPlaceholder.get(`/comments/${id}`);
+  const response = await jsonPlaceholder.get(`/comments`);
   dispatch({ type: 'FETCH_COMMENT', payload: response.data});
 };
