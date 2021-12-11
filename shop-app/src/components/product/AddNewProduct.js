bb55d33540664c79dce8dc34dd8124d19d5c7944
Heneros <punk60826@gmail.com>
2021-12-11 02:46:22 +0200

    form created

--- shop-app/src/components/product/AddNewProduct.js
+++ shop-app/src/components/product/AddNewProduct.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Field, reduxForm} from 'redux-form';
 
 
-const AddNewProduct = () =>{
-    return <div className="ui container">Add New Product</div>
+class AddNewProduct extends React.Component{
+     renderInput({input, label}){
+       return ( <div className='field'>
+           <label>{label}</label>
+           <input {...input} /> 
+           </div>
+           );
+     }
+
+     onSubmit(formValues){
+         console.log(formValues)
+     }
+
+    render(){
+   console.log(this.props);
+   return( 
+       <form 
+       onSubmit={this.props.handleSubmit(this.onSubmit)}
+       className='ui form'>
+           <Field name="title" component={this.renderInput} label="Enter Name of Product" />
+           <Field name="description" component={this.renderInput} label="Enter Description" />
+           <button>Submit</button>
+     </form>
+        )
+   }
 }
 
-export default AddNewProduct;
+export default reduxForm({
+    form: 'addProduct'
+})(AddNewProduct);
--- shop-app/src/components/reducers/index.js
+++ shop-app/src/components/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import commentsReducer from './commentsReducer';
 import productsReducer from './productsReducer';
 import usersReducers from './usersReducers';
-
+import { reducer as formReducer } from 'redux-form';
+ 
 export default combineReducers({
      products: productsReducer,
      users: usersReducers,
-     comments: commentsReducer
+     comments: commentsReducer,
+     form: formReducer
 });
