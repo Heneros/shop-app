4e2766bc98a94eddb8c40a68a12e4b89433498f8
Heneros <punk60826@gmail.com>
2021-12-11 03:28:39 +0200

    show errors

--- shop-app/src/components/product/AddNewProduct.js
+++ shop-app/src/components/product/AddNewProduct.js
@@ -3,16 +3,33 @@ import { Field, reduxForm} from 'redux-form';
 
 
 class AddNewProduct extends React.Component{
-     renderInput({input, label}){
-       return ( <div className='field'>
+    renderError({error, touched}){
+        if(touched && error){
+            return (
+                <div className='ui error message'>
+                    <div className='header'>
+                        {error}
+                    </div>
+
+                </div>
+
+            );
+        }
+    }
+  
+    renderInput = ({input, label, meta}) =>{
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+     return ( <div className={className}>
            <label>{label}</label>
            <input {...input} /> 
+           {this.renderError(meta)}
            </div>
            );
      }
 
      onSubmit(formValues){
          console.log(formValues)
+        //  this.props.onSubmit(formValues);
      }
 
     render(){
@@ -20,15 +37,27 @@ class AddNewProduct extends React.Component{
    return( 
        <form 
        onSubmit={this.props.handleSubmit(this.onSubmit)}
-       className='ui form'>
+       className='ui form error'>
            <Field name="title" component={this.renderInput} label="Enter Name of Product" />
            <Field name="description" component={this.renderInput} label="Enter Description" />
-           <button>Submit</button>
+           <button className='ui button primary'>Submit</button>
      </form>
         )
    }
 }
+const validate = (formValues) =>{
+    const errors = {};
+
+    if(!formValues.title){
+        errors.title = 'You must enter a title';
+    }
+    if(!formValues.description){
+        errors.description = 'You must enter a description';
+    }
+    return errors;
+}
 
 export default reduxForm({
-    form: 'addProduct'
+    form: 'addProduct',
+    validate
 })(AddNewProduct);
