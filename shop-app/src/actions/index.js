b6601489e62dc227da9e4d370d02c62950d974ea
Heneros <punk60826@gmail.com>
2021-12-17 01:52:49 +0200

    actions created

--- shop-app/src/actions/index.js
+++ shop-app/src/actions/index.js
@@ -3,7 +3,15 @@ import _ from 'lodash';
 
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 // import addProductReducer from './addProductReducer';
-import { ADD_PRODUCT } from './types'
+import { 
+  ADD_PRODUCT,
+  CREATE_PRODUCT,
+  FETCH_PRODUCTS,
+  FETCH_PRODUCT,
+  DELETE_PRODUCT,
+  EDIT_PRODUCT
+
+ } from './types'
 
 
 export const fetchProducts =  () => async dispatch => {
@@ -22,4 +30,25 @@ export const fetchComment = id => async dispatch =>{
 export const createProduct = formValues => async dispatch =>{
   const response = await products.post('/product', formValues);
   dispatch({type: ADD_PRODUCT, payload: response})
+}
+
+
+export const fetchProductS = () => async dispatch =>{
+  const response = await products.get('/product');
+  dispatch({type: FETCH_PRODUCTS, payload: response})
+}
+
+export const fetchProduct = (id) => async dispatch =>{
+  const response = await products.get(`/products/${id}`);
+  dispatch({type: FETCH_PRODUCT, payload: response})
+}
+
+export const editProduct = (id, formValues) => async dispatch =>{
+  const response = await products.put(`/products/${id}`, formValues);
+  dispatch({type: EDIT_PRODUCT, payload: response})
+}
+
+export const deleteProduct = (id) => async dispatch =>{
+   await products.dlete(`/products/${id}`);
+  dispatch({type: DELETE_PRODUCT, payload: id})
 }
--- shop-app/src/actions/types.js
+++ shop-app/src/actions/types.js
@@ -1,1 +1,7 @@
 export const ADD_PRODUCT = 'ADD_PRODUCT';
+export const CREATE_PRODUCT = 'ADD_PRODUCT';
+export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+export const FETCH_PRODUCT = 'FETCH_PRODUCT';
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const EDIT_PRODUCT = 'EDIT_PRODUCT';
+
