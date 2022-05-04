3ded6f3f49ae52694ce324dec82cfb2b39984149
Heneros <punk60826@gmail.com>
2022-05-05 01:58:52 +0300

    upload file with functional component

--- shop-app/src/components/product/ProductForm.js
+++ shop-app/src/components/product/ProductForm.js
@@ -1,5 +1,5 @@
 import axios, {post} from 'axios';
-import React  from 'react';
+import React, { useState }  from 'react';
 import { Field, reduxForm } from 'redux-form';
 // import { useForm } from "react-hook-form";
 import { connect } from 'react-redux';
@@ -8,80 +8,21 @@ import FileUpload from './FileUpload/FileUpload';
 
 
 
-class ProductForm extends React.Component{
+function ProductForm() {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-            files: '', 
-            setFiles: ''
-        };
-	}
-	
-	
- 
-
-
-    renderError({error, touched}){
-        if(touched && error){
-            return (
-                <div className='text-danger'>
-                        {error}
-                </div>
-            );
-        }
+   const [files, setFiles] = useState([{
+       name: 'FileName.png'
+   }])
+    const removeFile = (filename, files) =>{
+        setFiles(files.filter(file => file.name !== filename))
     }
-
-
-    renderInput = ({input, label, meta}) =>{
-    const className = `mb-3 ${meta.error && meta.touched ? 'error' : ''}`;
-     return ( <div className={className}>
-           <label className="form-label">{label}</label>
-           <input required className="form-control " {...input} /> 
-           {this.renderError(meta)}
-           </div>
-           );
-     }
-
-
-
-  onSubmit = (formValues, data) => {
-         this.props.onSubmit(formValues);
-         console.log(data);
-     }
-     setFiles = ()=>{
-
-     }
-    render(){
- 
+    console.log(files);
    return( 
-       <form 
-       onSubmit={this.props.handleSubmit(this.onSubmit)}
-       >
-           <Field name="title" component={this.renderInput}   label="Enter Name of Product" />
-           <Field name="description" component={this.renderInput} label="Enter Description" />
- 
-           <FileUpload files="files" onClick={this.setFiles} />
-
-     </form>
+       <>
+             <p className='title'>Upload file</p>
+            <FileUpload files={files} setFiles={setFiles} removeFile={removeFile} />
+       </>
         )
-   }
 }
-const validate = (formValues) =>{
-    const errors = {};
 
-    if(!formValues.title){
-        errors.title = 'You must enter a title';
-    }
-    if(!formValues.description){
-        errors.description = 'You must enter a description';
-    }
-    if(!formValues.test){
-        errors.test = 'You must load file';
-    }
-    return errors;
-}
-export default reduxForm({
-    form: 'productForm',
-    validate,
-})(ProductForm);
+export default ProductForm;
