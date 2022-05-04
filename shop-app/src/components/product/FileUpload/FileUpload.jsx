7e5653b73ecb7519168a81578201fe5df1e3e768
Heneros <punk60826@gmail.com>
2022-05-04 03:44:37 +0300

    updated file ProductForm and create file FileUpload.jsx

---
+++ shop-app/src/components/product/FileUpload/FileUpload.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function FileUpload({files, setFiles}) {
+ const uploadHandler = () => {}
+    return (
+    <>
+    
+    <div className="file-card">
+        <div className="file-inputs">
+            <input type="file" />
+        </div>
+    </div>
+    
+    </>
+  )
+}
--- shop-app/src/components/product/ProductForm.js
+++ shop-app/src/components/product/ProductForm.js
@@ -3,6 +3,7 @@ import React  from 'react';
 import { Field, reduxForm } from 'redux-form';
 // import { useForm } from "react-hook-form";
 import { connect } from 'react-redux';
+import FileUpload from './FileUpload/FileUpload';
 // import { register } from "../../actions";
 
 
@@ -11,74 +12,14 @@ class ProductForm extends React.Component{
 
 	constructor(props) {
 		super(props);
-		this.state = {file: '', msg: ''};
+		this.state = {
+            files: '', 
+            setFiles: ''
+        };
 	}
 	
-	onFileChange = (event) => {
-		this.setState({
-			file: event.target.files[0]
-		});
-	}
 	
-	uploadFileData = (event) => {
-		event.preventDefault();
-		this.setState({msg: ''});
-
-		let data = new FormData();
-		data.append('file', this.state.file);
-
-		fetch('http://localhost:3000/upload', {
-			method: 'POST',
-			body: data
-		}).then(response => {
-			this.setState({msg: "File successfully uploaded"});
-		}).catch(err => {
-			this.setState({error: err});
-		});
-
-	}
-	
-    // state = {
-    //     selectedFile: null
-    //   }; 
-      // onFileChange = event => {
-      //   this.setState({ selectedFile: event.target.files[0] });
-      
-      // };
-    // onFileUpload = () => {
-    //     const formData = new FormData();
-    //     formData.append(
-    //       "myFile",
-    //       this.state.selectedFile,
-    //       this.state.selectedFile.name
-    //     );
-    //     console.log(this.state.selectedFile);
-    //     axios.post("products/images", formData);
-    //   };
-
-    //   fileData = () => {
-    //     if (this.state.selectedFile) {
-    //       return (
-    //         <div>
-    //           <h2>File Details:</h2>
-    //         <p>File Name: {this.state.selectedFile.name}</p> 
-    //         <p>File Type: {this.state.selectedFile.type}</p>
-    //             <p>
-    //             Last Modified:{" "}
-    //             {this.state.selectedFile.lastModifiedDate.toDateString()}
-    //           </p>
-    //         </div>
-    //       );
-    //     } else {
-    //       return (
-    //         <div>
-    //           <br />
-    //           <h4>Choose before Pressing the Upload button</h4>
-    //         </div>
-    //       );
-    //     }
-    //   };
-
+ 
 
 
     renderError({error, touched}){
@@ -101,36 +42,27 @@ class ProductForm extends React.Component{
            </div>
            );
      }
-     renderImage = ({input, label, meta, isSelected, selectedFile}) =>{
-        const className = `mb-3 ${meta.error && meta.touched ? 'error' : ''}`;
-        return ( <div className={className}>
-            <label className="form-label">{label}</label>
-            {/* <input type="file" className="form-control" onChange={this.onFileChange} />
-            <input type="file" className="form-control" ref={register} /> */}
-           
-           </div>
-            );
 
-     }
 
 
   onSubmit = (formValues, data) => {
          this.props.onSubmit(formValues);
          console.log(data);
      }
+     setFiles = ()=>{
 
+     }
     render(){
  
    return( 
        <form 
        onSubmit={this.props.handleSubmit(this.onSubmit)}
        >
-           <Field name="title" component={this.renderInput}        label="Enter Name of Product" />
+           <Field name="title" component={this.renderInput}   label="Enter Name of Product" />
            <Field name="description" component={this.renderInput} label="Enter Description" />
-           {/* <Field name="image"  component={this.renderImage}      label="Load Image" /> */}
-           {/* <button>Submit</button> */}
-           <input onChange={this.onFileChange} type="file"></input>
-				   <button onClick={this.uploadFileData}>Upload</button>
+ 
+           <FileUpload files="files" onClick={this.setFiles} />
+
      </form>
         )
    }
@@ -149,7 +81,6 @@ const validate = (formValues) =>{
     }
     return errors;
 }
-//
 export default reduxForm({
     form: 'productForm',
     validate,
