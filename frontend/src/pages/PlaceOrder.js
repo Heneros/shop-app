ed724a6a101c8fa32751cecf199edb58d1521f2c
Heneros <punk60826@gmail.com>
2023-11-09 14:45:31 +0200

    PlaceOrder.js

--- frontend/src/pages/PlaceOrder.js
+++ frontend/src/pages/PlaceOrder.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useCreateOrderMutation } from '../redux/slices/orderApiSlice';
+import { toast } from 'react-toastify';
+import { clearCartItems } from '../redux/slices/cartSlice'
 
 export default function PlaceOrder() {
     const navigate = useNavigate();
     const cart = useSelector((state) => state.cart);
 
     const [createOrder, { isLoading, error }] = useCreateOrderMutation();
+    useEffect(() => {
+        if (!cart.shippingAddress.address) {
+            navigate('/shipping')
+        } else if (!cart.paymentMethod) {
+            navigate('/payment')
+        }
+    }, [cart.paymentMethod, cart.shippingAddress, navigate])
+
+    const dispatch = useDispatch();
+
+    const placeOrderHandler = async () => {
+        try {
+            const res = await createOrder({
+                orderItems: cart.cartItems,
+                shippingAddress: cart.shippingAddress,
+                paymentMethod: cart.paymentMethod,
+                itemsPrice: cart.itemsPrice,
+                shippingPrice: cart.shippingPrice,
+                taxPrice: cart.taxPrice,
+                totalPrice: cart.totalPrice,
+            }).unwrap();
+            dispatch(clearCartItems());
+            navigate(`/order/${res._id}`);
+        } catch (error) {
+            toast.error(error)
+        }
+    }
+
+
     return (
-        <div>PlaceOrder</div>
+        <div>
+
+        </div>
     )
 }
