e1fc4790e9a9f023821392cfbfe766f4305e77a3
Heneros <punk60826@gmail.com>
2024-01-01 01:28:20 +0200

    routeUpload.js

---
+++ backend/routes/routeUpload.js
@@ -0,0 +1,20 @@
+const expres = require("express");
+const multer = require("multer");
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename(req, file, cb) {
+        cb(
+            null,
+            `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+        );
+    },
+});
+
+function fileFilter(req, file, cb) {
+    const filetypes = /jpe?g|png|webp/;
+    const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+}
